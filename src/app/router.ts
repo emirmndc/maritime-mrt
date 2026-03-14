3ed790afb19af6dbeb7e7c0ba087aa5247b9a486
MARITIME <emir.mandacii@outlook.com>
2026-03-14 18:12:54 +0000

    Add try demo route

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,6 +1,7 @@
 export const appRoutes = {
   "/": { title: "MARITIME MRT" },
   "/app": { title: "MARITIME App" },
+  "/app/try-demo": { title: "Try Demo | MARITIME App" },
   "/app/voyages": { title: "Voyages | MARITIME App" },
   "/app/voyages/demo": { title: "Demo Voyage | MARITIME App" },
 } as const;
