544827edc0229400a68539684ce96441b829073a
MARITIME <emir.mandacii@outlook.com>
2026-04-05 13:37:01 +0000

    Update AppHomePage.tsx

--- src/app/AppHomePage.tsx
+++ src/app/AppHomePage.tsx
@@ -1,7 +1,7 @@
-<AppLink route="/app/generated-dashboard" tone="something">
+<AppLink route="/app/generated-dashboard">
   ...
 </AppLink>
 
-<AppLink route="/app/settlement" tone="something">
+<AppLink route="/app/settlement">
   ...
 </AppLink>
